4da9454fc02327ba688b270a466a5baeee67fc33
Morgarathftw <morgarathftw@gmail.com>
2016-09-29 19:42:50 +1300

    Turret + Bullet
    Turret doesnt shoot bullet right angle yet

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour
+{
+
+    public Vector3 _Velocity;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        GetComponent<Rigidbody2D>().velocity = _Velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBullet();
+    }
+
+    void MoveBullet()
+    {
+        //ansform.position = transform.position + _Velocity;
+    }
+
+    void OnCollisionEnter2D(Collision2D Col)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/TurretScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretScript : MonoBehaviour {
+
+    public GameObject ButtonTriggerObject;
+    Button ButtonTrigger;
+    public bool Inverted;
+    public float _fTimer = 2.5f;
+    public float _fBulletVelocity;
+    private Vector2 _fvBulletVelocity;
+    float _fTimeSince = 0.0f;
+
+    public GameObject Bullet;
+
+    public bool IsTurretOn;
+
+    // Use this for initialization
+    void Start () {
+   
+        if (Inverted == true)
+        {
+            IsTurretOn = false;
+        }
+        else
+        {
+            IsTurretOn = true;
+        }
+        ButtonTrigger = ButtonTriggerObject.GetComponent<Button>();
+
+        Debug.Log(_fvBulletVelocity);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Inverted)
+        {
+            if (ButtonTrigger.ButtonPressed)
+            {
+                IsTurretOn = true;
+            }
+            else
+            {
+                IsTurretOn = false;
+            }
+        }
+        else
+        {
+            if (ButtonTrigger.ButtonPressed)
+            {
+                IsTurretOn = false;
+            }
+            else
+            {
+                IsTurretOn = true;
+            }
+        }
+        if (IsTurretOn)
+        {
+            if (Time.time > _fTimeSince + _fTimer)
+            {
+                Debug.Log("sfdhsgf");
+                _fTimeSince = Time.time;
+                Vector3 _newVector = transform.position;
+                _newVector.x = _newVector.x + 1.0f;
+                GameObject Yes = Instantiate(Bullet, _newVector, transform.rotation) as GameObject;
+                Yes.GetComponent<Rigidbody2D>().velocity = _fvBulletVelocity;
+            }
+        }
+    }
+}
