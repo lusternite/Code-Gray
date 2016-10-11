a2a37330a2669ae5ffb8883c28952b5d35540d62
Morgarathftw <morgarathftw@gmail.com>
2016-10-11 22:46:38 +1300

    Fire
    Added fire
    Can fix death radius
    Can fix burn radius
    Can fix damage tick

---
+++ Assets/Scripts/FireScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireScript : MonoBehaviour {
+
+    public int _iFireDamage = 3;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay2D(Collider2D Col)
+    {
+        if (Col.gameObject.name == "Memory Upright Prefab(Clone)" )
+        {
+            Col.gameObject.GetComponent<MemoryScript>().TakeDamage(_iFireDamage);
+            Debug.Log("laser hit memory");
+        }
+    }
+}
