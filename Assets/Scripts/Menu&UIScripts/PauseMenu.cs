05a59cd3eceacfd3ec3489d1d69144b94cf20cba
L0xus <cstone1250@gmail.com>
2016-10-18 17:52:02 +1300

    Pause menu operable with up/down keys and space

--- Assets/Scripts/Menu&UIScripts/PauseMenu.cs
+++ Assets/Scripts/Menu&UIScripts/PauseMenu.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -7,11 +9,39 @@ public class PauseMenu : MonoBehaviour
     public GameObject ResumeButtonReference;
     public GameObject ToggleSoundButtonReference;
     public GameObject MenuButtonReference;
-    int CurrentlySelectedOption = 0;
+    public int CurrentlySelectedOption = 0;
+    public Sprite resumeOn;
+    public Sprite resumeOff;
+    public Sprite toggleOn;
+    public Sprite toggleOff;
+    public Sprite menuOn;
+    public Sprite menuOff;
+
+    GameObject[] buttons;
+    Sprite[] sprites;
 
     // Use this for initialization
     void Start ()
     {
+        ResumeButtonReference.GetComponent<Image>().sprite = resumeOn;
+        ToggleSoundButtonReference.GetComponent<Image>().sprite = toggleOff;
+        MenuButtonReference.GetComponent<Image>().sprite = menuOff;
+
+        buttons = new GameObject[3];
+        buttons[0] = ResumeButtonReference;
+        buttons[1] = ToggleSoundButtonReference;
+        buttons[2] = MenuButtonReference;
+
+        sprites = new Sprite[6];
+        sprites[0] = resumeOn;
+        sprites[1] = resumeOff;
+        sprites[2] = toggleOn;
+        sprites[3] = toggleOff;
+        sprites[4] = menuOn;
+        sprites[5] = menuOff;
+
+        Debug.Log(buttons[0]);
+
         if (!instance)
         {
             instance = this;
@@ -30,27 +60,52 @@ public class PauseMenu : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S) || Input.GetAxis("Vertical") < 0.0f)
+        if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S) || Input.GetAxis("Vertical") < -0.1f)
         {
+            buttons[CurrentlySelectedOption].GetComponent<Image>().sprite = sprites[CurrentlySelectedOption * 2 + 1];
             CurrentlySelectedOption += 1;
             CurrentlySelectedOption = CurrentlySelectedOption % 3;
+            buttons[CurrentlySelectedOption].GetComponent<Image>().sprite = sprites[CurrentlySelectedOption * 2];
         }
 
-        else if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W) || Input.GetAxis("Vertical") > 0.0f)
+        else if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W) || Input.GetAxis("Vertical") > 0.1f)
         {
+            buttons[CurrentlySelectedOption].GetComponent<Image>().sprite = sprites[CurrentlySelectedOption * 2 + 1];
             CurrentlySelectedOption -= 1;
             if (CurrentlySelectedOption < 0)
             {
                 CurrentlySelectedOption = 2;
             }
+            buttons[CurrentlySelectedOption].GetComponent<Image>().sprite = sprites[CurrentlySelectedOption * 2];
         }
-        else if (Input.GetKeyUp(KeyCode.Space) || Input.GetAxis("Vertical") > 0.0f)
+        //else if (Input.GetKeyUp(KeyCode.Space) || Input.GetAxis("Vertical") > 0.0f)
+        //{
+        //    buttons[CurrentlySelectedOption].GetComponent<Image>().sprite = sprites[CurrentlySelectedOption * 2 + 1];
+        //    CurrentlySelectedOption -= 1;
+        //    if (CurrentlySelectedOption < 0)
+        //    {
+        //        CurrentlySelectedOption = 2;
+        //    }
+        //    buttons[CurrentlySelectedOption].GetComponent<Image>().sprite = sprites[CurrentlySelectedOption * 2];
+        //}
+
+        if (Input.GetKeyUp(KeyCode.Space))
         {
-            CurrentlySelectedOption -= 1;
-            if (CurrentlySelectedOption < 0)
+            GameManager manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+            if (CurrentlySelectedOption == 0)
             {
-                CurrentlySelectedOption = 2;
+                manager.TogglePause();
             }
+            if (CurrentlySelectedOption == 1)
+            {
+                manager.ToggleSound();
+            }
+            if (CurrentlySelectedOption == 2)
+            {
+                manager.TogglePause();
+                manager.GoToMenu();
+            }
+            Debug.Log("hi");
         }
     }
 
